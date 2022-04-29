f195e0c967d88c78a77bd0fd16960fdbe1d7cf5a
Adeet Patel <adeet.patel@gmail.com>
2022-04-28 21:48:29 -0400

    New approach for train/val/test split

--- train_test_validation_script.py
+++ train_test_validation_script.py
@@ -1,31 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+# ====================================================================================
+# IMPORT LIBRARIES
+# ====================================================================================
 
 import numpy as np
+np.random.seed(0)
 import pandas as pd
-import matplotlib.pyplot as plt
-import dask.dataframe as dd
-import dask.bag as db
-import tqdm 
+import itertools
 
-from distributed import Client
-#from dask_jobqueue import SLURMCluster
-from IPython.display import display
-
-import os
-from glob import glob
-
-
+# ====================================================================================
+# IMPORT DATA + DATA PROCESSING
+# ====================================================================================
 
 # Import the ratings dataset
-ratings_df = pd.read_csv('ratings.csv').sort_values(by="timestamp").reset_index(drop=True)
-#print("This dataset contains {} rows and {} columns.".format(ratings_df.shape[0], ratings_df.shape[1]))
-#ratings_df.head()
-
-
+ratings_df = pd.read_csv('ml-latest/ratings.csv')
+ratings_df = ratings_df.sort_values(by="timestamp")
 
 # Count the number of ratings for each movie
 num_ratings_per_movie = ratings_df.groupby("movieId").count()["rating"]
@@ -35,47 +23,71 @@ ratings_df = ratings_df.join(num_ratings_per_movie, on = "movieId", rsuffix="_co
 ratings_df = ratings_df[ratings_df["rating_count"] >= 5]
 ratings_df.drop(columns=["rating_count"], inplace=True)
 
-# Compute how many times each movie has been rated
-#print(ratings_df["movieId"].value_counts(ascending=True))
+# ====================================================================================
+# TRAIN/VAL/TEST SPLIT
+# ====================================================================================
 
+# An array containing the IDs of all users
+all_users = ratings_df['userId'].unique()
 
-# ## Training-validation-test split
+# Shuffle the array, because we want to randomly split the users into training/val/test sets
+np.random.shuffle(all_users)
 
-# Split each user's data into training-validation-test set. First, we will separate 20% of the users to be part of the test set. Then for each remaining user, use 80% of their ratings for training and 20% for validation. We do this so that our recommender system generalizes across different kinds of users.
+# Out of all the users we have, we put the first 60% of them in the training set 
+train_size = int(0.6 * len(all_users))
+train_users = all_users[:train_size]
+train_df = ratings_df[ratings_df['userId'].isin(train_users)]
 
+# Put the next 20% in the validation set
+val_size = train_size + int(0.2 * len(all_users))
+val_users = all_users[train_size:val_size]
+val_df = ratings_df[ratings_df['userId'].isin(val_users)]
 
-train_users = np.random.choice(ratings_df['userId'].unique(), int(len(ratings_df['userId'].unique()) * 0.8), replace=False)
+# Put the last 20% in the testing set
+test_users = all_users[val_size:]
+test_df = ratings_df[ratings_df['userId'].isin(test_users)]
 
-train_val_df = ratings_df[ratings_df['userId'].isin(train_users)]
-test_df = ratings_df[~ratings_df['userId'].isin(train_users)]
+print("Sanity check: {} ?= {}".format(len(ratings_df), len(train_df) + len(val_df) + len(test_df)))
+print(len(train_df), len(val_df), len(test_df))
 
+val_group = val_df.groupby('userId')
+test_group = test_df.groupby('userId')
 
+# A list of dataframes, where each dataframe has the ratings for one user
+val_df_groups = list(map(lambda user: val_group.get_group(user), val_users))
+test_df_groups = list(map(lambda user: test_group.get_group(user), test_users))
 
-# For each user, compute the number of ratings they submitted
-num_ratings_per_user = train_val_df.groupby('userId').count()['rating']
+# For each validation user, get a chunk of their data
+chunk_frac = 0.3
+train_subsets = list(map(lambda user_data: user_data[:int(chunk_frac*len(user_data))], val_df_groups))
 
+# Move these chunks into the training set
+train_df = pd.concat([train_df] + train_subsets)
 
-# Create training and validation sets for each user
-train_df, val_df = pd.DataFrame(), pd.DataFrame()
+# Move these chunks out of the validation set
+indices_to_drop = map(lambda subset: subset.index, train_subsets)
+indices_to_drop = list(itertools.chain.from_iterable(indices_to_drop))
+val_df = val_df.drop(indices_to_drop)
 
-for userId, num_ratings in tqdm.tqdm_notebook(list(zip(num_ratings_per_user.index, num_ratings_per_user))):
-    # Get all the ratings for this user
-    user_ratings = train_val_df[train_val_df['userId'] == userId].reset_index(drop=True)
-    
-    # Make the first 80% of this user's ratings the training set
-    index_train = int(0.8*num_ratings)
-    user_train = user_ratings.loc[:index_train-1, :]
-    
-    # Make the other 20% of this user's ratings the validation set
-    user_val = user_ratings.loc[index_train:, :]
-    
-    # Add this user's individual training and validation sets to the unified training and
-    # validation sets, respectively
-    train_df = pd.concat([train_df, user_train], axis=0)
-    val_df = pd.concat([val_df, user_val], axis=0)
+# For each testing user, get a chunk of their data
+chunk_frac = 0.3
+train_subsets = list(map(lambda user_data: user_data[:int(chunk_frac*len(user_data))], test_df_groups))
 
+# Move these chunks into the training set
+train_df = pd.concat([train_df] + train_subsets)
 
-train_df.to_csv("train_df_full.csv")
-val_df.to_csv("val_df_full.csv")
-test_df.to_csv("test_df_full.csv")
+# Move these chunks out of the testing set
+indices_to_drop = map(lambda subset: subset.index, train_subsets)
+indices_to_drop = list(itertools.chain.from_iterable(indices_to_drop))
+test_df = test_df.drop(indices_to_drop)
 
+print("Sanity check: {} ?= {}".format(len(ratings_df), len(train_df) + len(val_df) + len(test_df)))
+print(len(train_df), len(val_df), len(test_df))
+
+# ====================================================================================
+# EXPORT DATASETS
+# ====================================================================================
+
+train_df.to_csv("ratings-big-train.csv")
+val_df.to_csv("ratings-big-val.csv")
+test_df.to_csv("ratings-big-test.csv")
